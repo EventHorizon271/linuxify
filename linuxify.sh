fdd08a8e7c152d7e116f50071b1c668c1db261be
Travis Sharpe <travis.sharpe@gmail.com>
2019-02-24 16:50:12 -0700

    Removed anything specifically MacOS related

--- linuxify.sh
+++ linuxify.sh
@@ -3,119 +3,45 @@
 set -euo pipefail
 
 linuxify_check_os() {
-  if ! [[ "$OSTYPE" =~ darwin* ]]; then
-      echo "This is meant to be run on macOS only"
+  if ! [[ "$OSTYPE" =~ linux-gnu ]]; then
+      echo "This is meant to be run on Linux only"
       exit
   fi
 }
 
-linuxify_check_brew() {
-  if ! command -v brew > /dev/null; then
-      echo "Homebrew not installed!"
-      echo "In order to use this script please install homebrew from https://brew.sh"
-      exit
-  fi
+linuxify_update_os() {
+    sudo apt update
+    sudo apt upgrade
+    sudo apt dist-upgrade
 }
 
-linuxify_formulas=(
-    # GNU programs non-existing in macOS
-    "watch"
-    "wget"
-    "wdiff --with-gettext"
-    "gdb"
-    "autoconf"
-
-    # GNU programs whose BSD counterpart is installed in macOS
-    "coreutils"
-    "binutils"
-    "diffutils"
-    "ed --with-default-names"
-    "findutils --with-default-names"
-    "gawk"
-    "gnu-indent --with-default-names"
-    "gnu-sed --with-default-names"
-    "gnu-tar --with-default-names"
-    "gnu-which --with-default-names"
-    "grep --with-default-names"
-    "gzip"
-    "screen"
-
-    # GNU programs existing in macOS which are outdated
-    "bash"
-    "emacs"
-    "gpatch"
-    "less"
-    "m4"
-    "make --with-default-names"
-    "nano"
-    "bison"
-
-    # BSD programs existing in macOS which are outdated
-    "flex"
-
-    # Other common/preferred programs in GNU/Linux distributions
-    "libressl"
-    "file-formula"
-    "git"
-    "openssh"
-    "perl"
-    "python"
-    "rsync"
-    "unzip"
-    "vim --override-system-vi"
+linuxify_packages=(
+    apt-utils
+    apt-file
+    command-not-found
 )
 
 linuxify_install() {
     linuxify_check_os;
-    linuxify_check_brew;
+    linuxify_update_os;
 
-    # Install all formulas
-    for (( i=0; i<${#linuxify_formulas[@]}; i++ )); do
-        brew install ${linuxify_formulas[i]}
-    done
-
-    # Change default shell to brew-installed /usr/local/bin/bash
-    grep -qF '/usr/local/bin/bash' /etc/shells || echo '/usr/local/bin/bash' | sudo tee -a /etc/shells > /dev/null
-    chsh -s /usr/local/bin/bash
-
-    # gdb requires special privileges to access Mach ports.
-    # One can either codesign the binary as per https://sourceware.org/gdb/wiki/BuildingOnDarwin
-    # Or, on 10.12 Sierra or later with SIP, declare `set startup-with-shell off` in `~/.gdbinit`:
-    grep -qF 'set startup-with-shell off' ~/.gdbinit || echo 'set startup-with-shell off' | tee -a ~/.gdbinit > /dev/null
-
-    # Make changes to PATH/MANPATH/INFOPATH/LDFLAGS/CPPFLAGS
-    cp .linuxify ~/.linuxify
-    echo "Add '[[ "$OSTYPE" =~ ^darwin ]] && [ -f ~/.linuxify ] && source ~/.linuxify' to your ~/.bashrc, ~/.zshrc or your shell's equivalent config file"
+    # Install all packages
+    sudo apt install -y ${linuxify_packages[@]}
 }
 
 linuxify_uninstall() {
     linuxify_check_os;
-    linuxify_check_brew;
 
-    # Remove gdb fix
-    sed -i.bak '/set startup-with-shell off/d' ~/.gdbinit && rm ~/.gdbinit.bak
-
-    # Change default shell back to macOS /bin/bash
-    sudo sed -i.bak '/\/usr\/local\/bin\/bash/d' /etc/shells && sudo rm /etc/shells.bak
-    chsh -s /bin/bash
-
-    # Uninstall all formulas in reverse order
-    for (( i=${#linuxify_formulas[@]}-1; i>=0; i-- )); do
-        brew uninstall $(echo "${linuxify_formulas[i]}" | cut -d ' ' -f1)
-    done
-
-    # Remove changes to PATH/MANPATH/INFOPATH/LDFLAGS/CPPFLAGS
-    rm -f ~/.linuxify
-    echo "Remove '[[ "$OSTYPE" =~ ^darwin ]] && [ -f ~/.linuxify ] && source ~/.linuxify' from your ~/.bashrc, ~/.zshrc or your shell's equivalent config file"
+    sudo apt uninstall ${linuxify_packages[@]}
 }
 
 linuxify_info() {
     linuxify_check_os;
-    linuxify_check_brew;
 
-    for (( i=0; i<${#linuxify_formulas[@]}; i++ )); do
+    for (( i=0; i<${#linuxify_packages[@]}; i++ )); do
         echo "==============================================================================================================================="
-        brew info ${linuxify_formulas[i]}
+        echo
+        sudo apt show ${linuxify_packages[i]}
     done
 }
 
