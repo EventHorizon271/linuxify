93ca5b8723ba5e7d93724cf94506b3c302899030
Travis Sharpe <travis.sharpe@protonmail.com>
2019-09-02 14:40:58 -0600

    Install .Net Core

--- linuxify.sh
+++ linuxify.sh
@@ -103,6 +103,7 @@ install_packages() {
 
     # Install Developer packages
     install_azuredatastudio
+    install_dotnetcore
     install_golang
     install_rust
 
@@ -299,6 +300,23 @@ install_azuredatastudio() {
     local url="https://go.microsoft.com/fwlink/?linkid=2100672"
 
     install_package "$name" "./$package" "$url"
+}
+
+install_dotnotecore() {
+    local name=".Net Core"
+    local version="2.2"
+    local package="dotnet-sdk-$version"
+
+    show_message "Install $name"
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+    sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+    wget -q https://packages.microsoft.com/config/debian/9/prod.list
+    sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+    sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+    sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+    sudo apt-get install apt-transport-https
+    sudo apt-get update
+    sudo apt-get install "$package"
 }
 
 install_rust() {
